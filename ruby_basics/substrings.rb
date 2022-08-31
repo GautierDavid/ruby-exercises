bef6a9f426444ffae5cae598f0ad16a8320d3042
Gautier DAVID <gautier.d.91@gmail.com>
2022-08-31 16:42:48 +0200

    Refactor method using #scan and #to_h

--- ruby_basics/substrings.rb
+++ ruby_basics/substrings.rb
@@ -1,10 +1,10 @@
 dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
 
 def substrings(str, arr)
-    arr.map do |el| 
-        el if str.downcase.include? el
-    end.compact
 
+  arr.map { |word| str.downcase.scan(word.downcase) } # scan return a 2d array of the el that match with string 
+        .filter {|array| !array.empty?} # return an array without the empty array
+        .to_h { |array| [array[0], array.length] } # transform the array in an hash, taking the first value of the subArray as key and length of the subarray as value
 end
 
-p substrings("Howdy partner, sit down! How's it going?", dictionary)
+p substrings("below", dictionary)
